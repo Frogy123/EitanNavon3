0b6997b244d8fc74782dd735a02946f47c1276c6
Frogy <thefrogeitan@gmail.com>
2024-01-10 15:38:06 +0200

    Final

--- app/src/main/java/com/example/eitannavon3/MainActivity.java
+++ app/src/main/java/com/example/eitannavon3/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.eitannavon3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
+    int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        counter = 0;
+        btn1 = findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(counter < 6){
+                    counter ++;
+                    btn1.setText("this is click number " + counter);
+                } else{
+                    btn1.setText("Enough to click. go to new start!");
+                }
+            }
+        });
     }
 }
